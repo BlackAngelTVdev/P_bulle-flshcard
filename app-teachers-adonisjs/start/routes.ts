bc7ac0e5f1852f968b3153959760d1a26368ac24
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-01-21 13:48:33 +0100

    Feat(step8): Ajout
    [20][DONE]
    langue, controlleur, validateur

--- app-teachers-adonisjs/start/routes.ts
+++ app-teachers-adonisjs/start/routes.ts
@@ -13,3 +13,5 @@ import router from '@adonisjs/core/services/router'
 router.get('/', [TeachersController, 'index']).as('home')
 router.get('/teacher/:id/show', [TeachersController, 'show']).as('teacher.show')
 router.delete('/teacher/:id/destroy', [TeachersController, 'destroy']).as('teacher.destroy')
+router.get('/teacher/add', [TeachersController, 'create']).as('teacher.create')
+router.post('/teacher/add', [TeachersController, 'store']).as('teacher.store')
