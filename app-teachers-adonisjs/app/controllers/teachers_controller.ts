90ce7de0ef40944d74c9b53e4ef57057eb54cbf8
Damien Rochat <156111574+BlackAngelTVdev@users.noreply.github.com>
2026-01-21 13:28:51 +0100

    Feat(setp7): delet and flash
    [15][DONE]
    Ajout des flash, delet
    Ajout des forms pour les delet

--- app-teachers-adonisjs/app/controllers/teachers_controller.ts
+++ app-teachers-adonisjs/app/controllers/teachers_controller.ts
@@ -5,15 +5,15 @@ export default class TeachersController {
   * Display a list of resource
   */
   async index({ view }: HttpContext) {
-  const teachers = await Teacher.query()
-    .orderBy('lastname', 'asc')
-    .orderBy('firstname', 'asc')
+    const teachers = await Teacher.query()
+      .orderBy('lastname', 'asc')
+      .orderBy('firstname', 'asc')
 
-  // On transforme les modèles en objets simples
-  const teachersJSON = teachers.map((teacher) => teacher.serialize())
+    // On transforme les modèles en objets simples
+    const teachersJSON = teachers.map((teacher) => teacher.serialize())
 
-  return view.render('pages/home', { teachers: teachersJSON })
-}
+    return view.render('pages/home', { teachers: teachersJSON })
+  }
   /**
   * Display form to create a new record
   */
@@ -25,28 +25,41 @@ export default class TeachersController {
   /**
   * Show individual record
   */
-async show({ params, view }: HttpContext) {
-  const teacher = await Teacher.query()
-    .where('id', params.id)
-    .preload('section')
-    .firstOrFail()
+  async show({ params, view }: HttpContext) {
+    const teacher = await Teacher.query()
+      .where('id', params.id)
+      .preload('section')
+      .firstOrFail()
 
-  return view.render('pages/teachers.edge', {
-    title: "Détail d'un enseignant", 
-    teacher: teacher.serialize() // Toujours mieux !
-  })
-}
+    return view.render('pages/teachers.edge', {
+      title: "Détail d'un enseignant",
+      teacher: teacher.serialize() // Toujours mieux !
+    })
+  }
   /**
   * Edit individual record
   */
-  async edit({ params }: HttpContext) {}
+  async edit({ params }: HttpContext) { }
 
   /**
   * Handle form submission for the edit action
   */
-  async update({ params, request }: HttpContext) {}
+  async update({ params, request }: HttpContext) { }
   /**
   * Delete record
   */
-  async destroy({ params }: HttpContext) {}
+  async destroy({ params, session, response }: HttpContext) {
+
+    const teacher = await Teacher.findOrFail(params.id)
+
+    await teacher.delete()
+
+    session.flash(
+      'success',
+      `L'enseignant ${teacher.lastname} ${teacher.firstname} a été supprimé avec
+succès !`
+    )
+    // Redirige l'utilisateur sur la home
+    return response.redirect().toRoute('home')
+  }
 }
